6fd0b229d325edef7ec60e5553a1c4393f4c02fb
paraskapoor760-hue <paraskapoor760@gmail.com>
2025-11-08 08:14:07 -0800

    Add rectangle area calculation

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+# Exercise 2 - Fixing a Semantic Error
+# The correct area formula for a rectangle is: area = side_1 * side_2
+
+print("There is a rectangle, of which one side is 3 and the other side is 4.")  
+side_1 = 3
+side_2 = 4
+print(f"I think the area of the rectangle is: {side_1 * side_2}.")
